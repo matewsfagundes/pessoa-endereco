786850f3678983bf24519848364d62cad9f8b27f
Matheus Fagundes <matewsfagundes@gmail.com>
2023-09-04 20:59:22 -0300

    inicia projeto

---
+++ src/main/java/br/com/Attornatus/cadastropessoaendereco/CadastroPessoaEnderecoApplication.java
@@ -0,0 +1,13 @@
+package br.com.Attornatus.cadastropessoaendereco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroPessoaEnderecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroPessoaEnderecoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/Attornatus/cadastropessoaendereco/CadastroPessoaEnderecoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.Attornatus.cadastropessoaendereco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroPessoaEnderecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
