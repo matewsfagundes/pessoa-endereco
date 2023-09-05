4617637777039ad41f65d16973cc66d0755b01e8
Matheus Fagundes <matewsfagundes@gmail.com>
2023-09-04 22:25:08 -0300

    ajusta mapeamneto UUID

--- src/main/java/br/com/Attornatus/cadastropessoaendereco/CadastroPessoaEnderecoApplication.java
+++ src/main/java/br/com/Attornatus/cadastropessoaendereco/CadastroPessoaEnderecoApplication.java
@@ -18,5 +18,4 @@ public class CadastroPessoaEnderecoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CadastroPessoaEnderecoApplication.class, args);
 	}
-
 }
---
+++ src/main/java/br/com/Attornatus/cadastropessoaendereco/pessoa/domain/pessoa.java
@@ -0,0 +1,37 @@
+package br.com.Attornatus.cadastropessoaendereco.pessoa.domain;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Entity
+public class pessoa {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, unique = true, nullable = false)
+	private UUID idPessoa;
+	@NotBlank
+	private String nomeCompleto;
+	@NotNull
+	private LocalDate dataNascimento;
+	
+	private pessoa(UUID idCliente, @NotBlank String nomeCompleto, @NotNull LocalDate dataNascimento) {
+		this.idPessoa = idCliente;
+		this.nomeCompleto = nomeCompleto;
+		this.dataNascimento = dataNascimento;
+	}
+
+}
