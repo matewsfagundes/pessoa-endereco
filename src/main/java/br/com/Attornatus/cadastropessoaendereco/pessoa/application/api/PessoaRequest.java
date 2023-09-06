1e4b6e2ddaf4ea7cb27c0a1402d3091a7a4723c0
Matheus Fagundes <matewsfagundes@gmail.com>
2023-09-05 23:17:42 -0300

    implementa Request e Response

--- src/main/java/br/com/Attornatus/cadastropessoaendereco/pessoa/application/api/PessoaRequest.java
+++ src/main/java/br/com/Attornatus/cadastropessoaendereco/pessoa/application/api/PessoaRequest.java
@@ -1,5 +1,16 @@
 package br.com.Attornatus.cadastropessoaendereco.pessoa.application.api;
 
+import java.time.LocalDate;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+
+@Value
 public class PessoaRequest {
+	@NotBlank
+	private String nomeCompleto;
+	@NotNull
+	private LocalDate dataNascimento;
 
 }
--- src/main/java/br/com/Attornatus/cadastropessoaendereco/pessoa/application/api/PessoaResponse.java
+++ src/main/java/br/com/Attornatus/cadastropessoaendereco/pessoa/application/api/PessoaResponse.java
@@ -1,5 +1,11 @@
 package br.com.Attornatus.cadastropessoaendereco.pessoa.application.api;
 
+import java.util.UUID;
+
+import lombok.Value;
+
+@Value
 public class PessoaResponse {
+	private UUID idPessoa;
 
 }
