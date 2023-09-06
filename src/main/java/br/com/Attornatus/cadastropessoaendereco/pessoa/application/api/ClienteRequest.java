45b079f0df5c4f987285deea6e23ccb51c9e4e96
Matheus Fagundes <matewsfagundes@gmail.com>
2023-09-05 21:30:39 -0300

    implementa contarto Api

---
+++ src/main/java/br/com/Attornatus/cadastropessoaendereco/pessoa/application/api/ClienteRequest.java
@@ -0,0 +1,5 @@
+package br.com.Attornatus.cadastropessoaendereco.pessoa.application.api;
+
+public class ClienteRequest {
+
+}
---
+++ src/main/java/br/com/Attornatus/cadastropessoaendereco/pessoa/application/api/pessoaAPI.java
@@ -0,0 +1,16 @@
+package br.com.Attornatus.cadastropessoaendereco.pessoa.application.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/pessoa")
+public interface pessoaAPI {
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	ClienteListResponse postCliente (@RequestBody ClienteRequest clienteRequest);
+}
